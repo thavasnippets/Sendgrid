3e7ebcb415c3a99ad1e8e012e0fb0caaa9915b2b
thava-snippets <thavaselvan@hotmail.com>
2020-04-10 14:32:09 -0700

    InitialCommit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SendGrid_StorageAccount.Helper;
+using SendGrid_StorageAccount.Models;
+
+namespace SendGrid_StorageAccount.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmailSender _emailSender;
+        
+        public HomeController(IEmailSender emailSender)
+        {
+
+            _emailSender = emailSender;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SendEmail()
+        {
+            EmailModel model = new EmailModel();
+            return View(model);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendEmail(EmailModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var emails = new List<string>();
+                emails.Add(model.Email);
+                await _emailSender.SendEmailAsync(emails, model.Subject, model.Message);
+            }
+            return View(model);
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Helper/SendGrid/EmailSender.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendGrid_StorageAccount.Helper
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+        public EmailSender(IConfiguration configuration, IOptions<EmailAuthOptions> optionsAccessor)
+        {
+            _configuration = configuration;
+            Options = optionsAccessor.Value;
+        }
+
+        public EmailAuthOptions Options { get; } //set only via Secret Manager
+
+        public Task SendEmailAsync(List<string> emails, string subject, string message)
+        {
+
+            return Execute(_configuration.GetSection("SENDGRID_API_KEY").Value, subject, message, emails);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, List<string> emails)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(_configuration.GetSection("SENDGRID_FROM_ADDRESS").Value, _configuration.GetSection("SENDGRID_FROM_NAME").Value),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+
+            foreach (var email in emails)
+            {
+                msg.AddTo(new EmailAddress(email));
+            }
+
+            Task response = client.SendEmailAsync(msg);
+            return response;
+        }
+    }
+}
